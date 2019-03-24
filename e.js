239cc5b04dcf155abfd737fb16dca8f860bb3755
1637923564 <1637923564@qq.com>
2019-03-24 10:47:59 +0800

    2019-03-24

--- e.js
+++ e.js
@@ -1,35 +1,4 @@
-import Cookies from "js-cookie";
-/**
- * @module 注册登录等功能的接口调用
- * @param {JQueryStatic} $
- */
-function UserTool($) {
-  // #region 实现 省略new关键字也能实例化对象
-  if (!this) {
-    return new StyleTool($)
-  }
-  //#endregion
-  /**
-   * 调用短信验证接口
-   * @method smsVeri
-   * @param {String} smsBtn 验证码获取按钮
-   * @param {String} phone 手机号输入框
-   */
-  this.smsVeri = function (smsBtn, phone, veri) {
-    let $smsBtn = $(smsBtn);
-    function smsBtnCallback(e) {
-      // 点击按钮之后，按钮样式将会变成不可点击样式，并实现60s的倒计时
-      let time = 60;
-      $smsBtn.unbind();
-      $smsBtn.css("background", "#eee");
-      let countdown = setInterval(() => {
-        time --;
-        $smsBtn.text(time);
-      }, 1000);
-      let phoneNum = $(phone).val();
-      // 生成一个[10000, 99999]的随机数
-      let veriCode = parseInt(Math.random() * (99999 - 10000 + 1)) + 10000;
-      // 短信验证接口调用和登录按钮绑定
+
       $.ajax({
         type: 'post',
         url: 'http://route.showapi.com/28-1',
@@ -45,11 +14,6 @@ function UserTool($) {
           alert("服务器繁忙，请稍后再重试!");
         },
         success: function (result) {
-          $("#layerLogWrap>.btn").css("cursor", "pointer");
-          setTimeout(() => {
-            $("#layerLogWrap>.btn").css("cursor", "no-drop");
-            $("#layerLogWrap>.btn").unbind();
-          }, 70000);
           $("#layerLogWrap>.btn").click(function(e) {
             if($(phone).val() === phoneNum && parseInt($(veri).val()) === veriCode) {
               // 验证手机号是否已经注册
@@ -95,15 +59,3 @@ function UserTool($) {
           })
         }
       });
-      setTimeout(() => {
-        clearInterval(countdown);
-        $smsBtn.text("获取验证码");
-        $smsBtn.css("background", "rgb(231,208,115)")
-        $smsBtn.click(smsBtnCallback);
-      }, 60000);
-    }
-    $smsBtn.click(smsBtnCallback);
-  }
-}
-
-export default UserTool;
